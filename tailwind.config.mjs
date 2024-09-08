7e1185355666f91c9ce93934ad25bc72b90e89a1
duskalor <duskalor1@gmail.com>
2024-09-08 11:25:41 -0500

    commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        link: '#E3A857',
+        headerandfooter: '#A13240',
+        ofertas: '#BBB8B8',
+        fondo: '#1E1E1E',
+      },
+    },
+  },
+  plugins: [],
+};
